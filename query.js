47392933efa1bd048e3f5de8e84ff1716bac2f07
Rocky Jake Santos <preymium5-ctrl@users.noreply.github.com>
2026-06-24 15:18:44 +0800

    Clean up repository: remove temporary debug captures, layout dumps, database backups, and scratch scripts

--- query.js
+++
@@ -1,9 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const db = new DatabaseSync('kotatsu-db');
-console.log("FAVOURITES MANGA DETAILS:");
-try {
-    const rows = db.prepare("SELECT CAST(m.manga_id AS TEXT) as manga_id_str, m.title, m.url, m.source, m.state, m.author FROM manga m JOIN favourites f ON m.manga_id = f.manga_id").all();
-    console.log(rows);
-} catch (e) {
-    console.error(e);
-}
--- slice_secure.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('secure.js', 'utf8');
-const start = 210000;
-const end = 213000;
-console.log(code.substring(start, end));
